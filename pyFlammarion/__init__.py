35f0eeb393e37692d0d1092dca3c65ca4c2c65c2
drAshcroft <brian.a.ashcroft@gmail.com>
2025-05-16 11:10:34 -0700

    dont get them all

--- pyFlammarion/__init__.py
+++ pyFlammarion/__init__.py
@@ -6,9 +6,9 @@ import types
 from . import ImagingTools as _imaging
 
 # Automatically populate __all__ with all functions and classes from AFM_Tools
-__all__ = [
-    name for name, obj in inspect.getmembers(_imaging)
-    if isinstance(obj, (types.FunctionType, type))
-]
+# __all__ = [
+#     name for name, obj in inspect.getmembers(_imaging)
+#     if isinstance(obj, (types.FunctionType, type))
+# ]
 # Package metadata
 __version__ = "0.1.0"
