5b7ed34f4163d2f4aab54daace84794af05001ef
drAshcroft <brian.a.ashcroft@gmail.com>
2025-05-16 11:01:04 -0700

    expose them all

--- pyFlammarion/__init__.py
+++ pyFlammarion/__init__.py
@@ -1,4 +1,14 @@
  
+from .AFM_Tools import *
 
+import inspect
+import types
+from . import AFM_Tools as _tools
+
+# Automatically populate __all__ with all functions and classes from AFM_Tools
+__all__ = [
+    name for name, obj in inspect.getmembers(_tools)
+    if isinstance(obj, (types.FunctionType, type))
+]
 # Package metadata
 __version__ = "0.1.0"
